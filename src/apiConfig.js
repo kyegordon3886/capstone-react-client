526c5ec6c1c99a2e02be3d4a278ab31207c1a50b
kyegordon3886 <kye.gordon.productions@gmail.com>
2022-05-24 12:22:22 -0500

    Added features such as like and comment
    Added buttons for like a comment
    Added some code for functionality, still not functionality
    Added some more styling

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://infinite-island-44375.herokuapp.com/',
+  production: 'https://infinite-island-44375.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
